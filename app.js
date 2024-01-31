4ebccdf1d0dce512554e18f5ba1716a293f0725f
robin nganga smith <saintofcookies.2021@gmail.com>
2024-01-31 15:30:06 +0300

    added variables, for loop and split function

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 function longestWord(str){
-return str;
+
+  let words = str.split(" ");
+  let longestWord = "";
+
+  for(let word of words){
+      if (word.length > longestWord.length){
+        longestWord = word
+
+      };
+      console.log(longestWord)
+  }
+
+return longestWord;
 }
+
+
+console.log(longestWord('this is a very long word'));
+console.log(longestWord('the creature is the largest word in this text'))
+
+
+
+
